e01b0d40db7349dc7fef536dea49f7ac390087c4
mguidoti <marcus.guidoti@gmail.com>
2020-12-10 22:26:11 -0300

    feat: Script for journal names transformation

---
+++ journals.py
@@ -0,0 +1,108 @@
+import pandas as pd
+from os import path
+
+
+def fix_journal(entry):
+  """[summary]
+
+  Args:
+      entry ([type]): [description]
+
+  Returns:
+      [type]: [description]
+  """
+  
+  fixes = {
+      'Zootaxa': [
+          'Zootaxa (Auckland. Print)', 
+          'Zootaxa (Online)'
+          ],
+      'Neotropical Entomology': [
+          'NEOTROPICAL ENTOMOLOGY', 
+          'Neotropical Entomology (Impresso)'
+          ],
+      'Revista Brasileira de Entomologia': [
+          'Revista Brasileira de Entomologia (Impresso)'
+          ],
+      'Zoologia': [
+          'Zoologia (Curitiba. Impresso)'
+          ],
+      'Acta Entomologica Musei Nationalis Pragae': [
+          'Acta entomologica Musei Nationalis Pragae', 
+          'ACTA ENTOMOLOGICA MUSEI NATIONALIS PRAGAE'
+          ],
+      'Revista Brasileira de Biociências': [
+          'Revista Brasileira de Biociências (Online)'
+          ],
+      'Cytogenetic and Genome Research': [
+          'Cytogenetic and Genome Research (Printed ed.)'
+          ],
+      'Cladistics': [
+          'Cladistics (Westport)'
+          ],
+      'Entomologica Americana': [
+          'Entomologica Americana (New York. Print)'
+          ],
+      'ZooKeys': [
+          'ZooKeys (Online)'
+          ],
+      'Memórias do Instituto Oswaldo Cruz': [
+          'Memórias do Instituto Oswaldo Cruz (Impresso)'
+          ],
+      'Zoomorphology': [
+          'Zoomorphology (Berlin. Print)'
+          ],
+      'Biota Neotropica': [
+          'Biota Neotropica (Edição em português. Impresso)',
+          'Biota Neotropica (Online. Edição em Inglês)'
+          ],
+      'The Florida Entomologist': [
+          'FLORIDA ENTOMOLOGIST'
+          ],
+      'Systematic Entomology': [
+        'Systematic Entomology (Print)'
+          ],
+      'Brazilian Journal of Biology': [
+        'BRAZILIAN JOURNAL OF BIOLOGY'
+          ],
+      'Beaufortia': [
+        'Beaufortia (Amsterdam)'
+          ],
+      'Zoomorphology': [
+        'ZOOMORPHOLOGY'
+          ],
+      'Zoologischer Anzeiger': [
+        'ZOOLOGISCHER ANZEIGER'
+          ],
+      'Brazilian Journal of Biology': [
+        'BRAZILIAN JOURNAL OF BIOLOGY (ONLINE)',
+        'BRAZILIAN JOURNAL OF BIOLOGY'
+          ],
+      'Environmental Entomology': [
+        'ENVIRONMENTAL ENTOMOLOGY'
+          ]
+    }
+
+  for key, value in fixes.items():
+      if entry in value:
+          return key
+      
+  return entry
+
+
+def transform(filepath, filename):
+  """
+
+  Args:
+      filepath (str): path where the input file is located
+      filename (str): name of the input file
+  """
+  
+  # Creating df, dealing with None
+  df = pd.read_csv(path.join(filepath, filename))
+
+  
+  df.Journal = df.Journal.apply(fix_journal)
+  
+  df.to_csv(path.join('output', '{}-transformed.csv'.format(
+                                                      filename[:-4])))
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import countries
 import authors
+import journals
 
 
 if __name__ == "__main__":
@@ -11,6 +12,7 @@ if __name__ == "__main__":
   collectors_file = 'grazia-festschrift-tb_stats-collector_name.csv'
   treat_cit_authors_file = ('grazia-festschrift-tb_stats-treatCit_cited_authors'
                             '.csv')  
+  journals_file = 'grazia-festschrift-basic_metadata.csv'
 
   # Starting transformation scripts
   countries.transform(input_folder, coauthors_countries_file)
@@ -18,3 +20,5 @@ if __name__ == "__main__":
   authors.transform(input_folder, taxon_authority_file)
   authors.transform(input_folder, collectors_file)
   authors.transform(input_folder, treat_cit_authors_file)
+  
+  journals.transform(input_folder, journals_file)
